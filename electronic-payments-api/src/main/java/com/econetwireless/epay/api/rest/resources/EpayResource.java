e370b4837544dd22ae0a9e4c38396faf4238ad1f
tinofay <tfzungunde@outlook.com>
2022-05-26 16:04:56 +0200

    Added Missing constructor in the EpayResource constructor as well as correcting the named query which was pointing to a wrong entity
    I could either use the constructor to instantiate the injected methods or the @Autowired Spring way of injecting beans.

--- electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
+++ electronic-payments-api/src/main/java/com/econetwireless/epay/api/rest/resources/EpayResource.java
@@ -17,10 +17,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/resources/services", headers = {"Accept=application/json", "Accept=application/xml", "Accept=text/xml"})
 public class EpayResource {
 
-    private EpayRequestProcessor epayRequestProcessor;
+    private final EpayRequestProcessor epayRequestProcessor;
 
 
-    private ReportingProcessor reportingProcessor;
+    private final ReportingProcessor reportingProcessor;
+
+    public EpayResource(EpayRequestProcessor epayRequestProcessor, ReportingProcessor reportingProcessor) {
+        this.epayRequestProcessor = epayRequestProcessor;
+        this.reportingProcessor = reportingProcessor;
+    }
 
     @GetMapping(value = "enquiries/{partnerCode}/balances/{mobileNumber}",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
